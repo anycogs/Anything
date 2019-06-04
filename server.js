c7d90460d9257067c5b552e51388a34600e9fe51
Senyan L <sel152@ucsd.edu>
2019-06-03 23:20:21 -0700

    time conversion added to article publish dates

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const axios = require("axios")
 const app = express();
-const request = require('request');
-const cheerio = require('cheerio');
 const bodyParser = require('body-parser');
 const path = require("path");
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('articles.db');
 const exphbs = require('express-handlebars');
 require('dotenv').config();
+const moment = require('moment')
 
 app.use(express.static('static_files'));
 app.engine('handlebars', exphbs({
@@ -20,27 +19,20 @@ app.set('view engine', 'handlebars');
 // access API keys by process.env.[API_key_var_name]
 const news_api_key = process.env.NEWS_API_KEY
 console.log(news_api_key)
-// Scraping not working yet, use a fake article
-/* const articleContent = {
-    'article': "Article: New York Times does not allow scraping of article, we just have the metadata from API call. Will look into other news API alternatives. A day after his father became the first monarch to abdicate the imperial throne of Japan in more than two centuries, the new emperor, Naruhito, on Wednesday received the sacred imperial regalia that represents his rightful succession to the world’s oldest monarchy.In an eye-blinkingly brief ceremony at the Imperial Palace, Naruhito, 59, officially succeeded Akihito, 85, an enormously popular monarch who brought the royal family much closer to the people as he emphasized a message of peace in a country haunted by the legacy of war."
-  }; */
 
 app.get("/", function (req, res) {
   //res.sendFile(path.join(__dirname, "/static_files/index.html"));
   res.render('index')
 });
+
 app.get("/archive", function (req, res) {
   db.all('SELECT * FROM saved_articles', (err, rows) => {
     console.log(rows);
-    /* const allLinks = rows.map(e => e.link);
-    const allTitles = rows.map(e => e.title);
-    const allDate = rows.map(e => e.date);
-    const allDescription = rows.map(e => e.description); */
-    //console.log(allTitles, allLinks, allDate, allDescription)
     res.render('archive', {articles: rows
     })
   });
 });
+
 app.get("/vocabulary", function (req, res) {
   db.all('SELECT * FROM vocabulary', (err, rows) => {
     console.log(rows);
@@ -77,7 +69,6 @@ app.get("/news/:continent", (req, res) => {
       break
     default:
       console.log("unidentified continent")
-      country = 'united states'
   }
 
   queryString = `https://newsapi.org/v2/everything?sortBy=publishedAt&language=en&q=${country}&apiKey=${news_api_key}`
@@ -87,8 +78,14 @@ app.get("/news/:continent", (req, res) => {
   
   axios.get(queryString)
     .then(function (data) {
-      console.log("*****************************************************************" + data.data.articles)
-      res.render('index', {articles: data.data.articles} )
+      console.log("*****************************************************************" + JSON.parse(JSON.stringify(data.data.articles)))
+      //convert time format
+      data.data.articles.forEach(function(doc){
+        console.log(doc.publishedAt)
+        doc.publishTime = moment(doc.publishedAt).format('YYYY-MM-DD HH:mm')
+        console.log(doc)
+      });
+      res.render('articles', {articles: data.data.articles} )
     })
     .catch(function (error) {
       /* console.log(error) */
@@ -98,21 +95,7 @@ app.get("/news/:continent", (req, res) => {
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-/* app.post('/articles', (req, res) => {
-  console.log(req.body)
-  let searchURL = req.body.url */
-  //Not able to scrape NYT article content, welp. Use fake data for now
-  /* request(searchURL, (err, res, body) => {
-      const $ = cheerio.load(body)
-      let bodyContent = $("section.meteredContent")
-      console.log(bodyContent.children)
-  }) */
 
- /*  res.send({
-    text: articleContent.article
-  })
-})
- */
 app.post('/archive', (req, res) => {
   console.log(req.body)
   // insert  into the archives database 
